2aa8dc66e50ce18a4745196c27f63ab15c8650f4
Andrei Dubinko <andrei.dubinko@krabutech.ee>
2023-08-17 14:28:46 +0300

    rolling back

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-const jsonServer = require("json-server");
+const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 81; // Изменили порт на 81
+const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port, () => {
-  const host = "www.bamofy.krabu.tech"; 
-  console.log(`Server is running at: http://${host}:${port}/`);
-
-}).on("error", (err) => {
-  console.error("Error starting server:", err);
-  process.exit(1); 
-});
+server.listen(port);
