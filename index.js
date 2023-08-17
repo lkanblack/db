521620c51da5a660726f2e542245c9b9ec3bc024
Andrei Dubinko <andrei.dubinko@krabutech.ee>
2023-08-17 14:16:04 +0300

    logging errors

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
+const port = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, (err) => {
+  if (err) {
+    console.error("Error starting server:", err);
+    process.exit(1); // Exit with an error code
+  } else {
+    console.log("Server listening on port " + port);
+  }
+});
