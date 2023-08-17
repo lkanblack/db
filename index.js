7d0f15d003760e557947eeaf10b2ec85b378dbef
Andrei Dubinko <andrei.dubinko@krabutech.ee>
2023-08-17 14:27:05 +0300

    changig port

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 81; // Изменили порт на 81
 
 server.use(middlewares);
 server.use(router);
 
 server.listen(port, () => {
   const host = "www.bamofy.krabu.tech"; 
-  console.log(`Server is running at: https://${host}:${port}/`);
+  console.log(`Server is running at: http://${host}:${port}/`);
 
 }).on("error", (err) => {
   console.error("Error starting server:", err);
